f880ffb8e0aee008d95176fdd1c3f6606819215d
Francesco <pham.francesco@gmail.com>
2020-05-06 21:50:07 +0200

    Implement partial ordering for Interval

--- src/interval/impl_cmp.rs
+++ src/interval/impl_cmp.rs
@@ -1,4 +1,5 @@
 use super::def::Interval;
+use std::cmp::Ordering;
 
 use fp::Float;
 
@@ -6,5 +7,18 @@ impl<BOUND: Float> PartialEq for Interval<BOUND> {
     #[inline]
     fn eq(&self, rhs: &Self) -> bool {
         self.is_nan() && rhs.is_nan() || self.lo == rhs.lo && self.hi == rhs.hi
+    }
+}
+
+impl<BOUND: Float> PartialOrd for Interval<BOUND> {
+    #[inline]
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        if self.lo > rhs.hi {
+            return Some(Ordering::Greater);
+        }
+        if self.hi < rhs.lo {
+            return Some(Ordering::Less);
+        }
+        None
     }
 }
