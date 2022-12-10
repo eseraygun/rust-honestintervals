7e0acae5c214c3ba44802e87cd4a4f25607b8ac9
Eser Aygün <eser.aygun@gmail.com>
2022-12-10 15:25:05 -0500

    Refactor `pow_multi`.

--- src/interval/impl_transc.rs
+++ src/interval/impl_transc.rs
@@ -12,8 +12,10 @@ impl<BOUND: Float> Interval<BOUND> {
         let (self_n, self_p) = self.split(BOUND::zero(precision));
         if !self_n.is_nan() {
             intervals.push(Self::new(
-                BOUND::min(self_n.lo.clone().pow_lo(rhs.clone()), self_n.hi.clone().pow_lo(rhs.clone())),
-                BOUND::max(self_n.lo.pow_hi(rhs.clone()), self_n.hi.pow_hi(rhs.clone())),
+                BOUND::min(self_n.lo.clone().pow_lo(rhs.clone()),
+                           self_n.hi.clone().pow_lo(rhs.clone())),
+                BOUND::max(self_n.lo.pow_hi(rhs.clone()),
+                           self_n.hi.pow_hi(rhs.clone())),
             ));
         }
         if !self_p.is_nan() {
@@ -45,8 +47,8 @@ impl<BOUND: Float> Interval<BOUND> {
     }
 
     fn pow_p_p_multi(self, rhs: Self) -> Vec<Self> {
-        assert!(self.sign_class().is_positive());
-        assert!(rhs.sign_class().is_positive());
+        assert!(self.sign_class().is_non_negative());
+        assert!(rhs.sign_class().is_non_negative());
 
         let precision = self.precision();
         let mut intervals = Vec::<Self>::new();
@@ -70,8 +72,8 @@ impl<BOUND: Float> Interval<BOUND> {
     }
 
     fn pow_p_n_multi(self, rhs: Self) -> Vec<Self> {
-        assert!(self.sign_class().is_positive());
-        assert!(rhs.sign_class().is_negative());
+        assert!(self.sign_class().is_non_negative());
+        assert!(rhs.sign_class().is_non_positive());
 
         let mut pos_intervals = self.pow_p_p_multi(-rhs);
         let res = pos_intervals
@@ -82,7 +84,7 @@ impl<BOUND: Float> Interval<BOUND> {
     }
 
     fn pow_p_a_multi(self, rhs: Self) -> Vec<Self> {
-        assert!(self.sign_class().is_positive());
+        assert!(self.sign_class().is_non_negative());
 
         let mut intervals = Vec::<Self>::new();
         let precision = rhs.precision();
@@ -97,7 +99,7 @@ impl<BOUND: Float> Interval<BOUND> {
     }
 
     fn pow_n_a_multi(self, rhs: Self) -> Vec<Self> {
-        assert!(self.sign_class().is_negative());
+        assert!(self.sign_class().is_non_positive());
 
         let mut intervals = Vec::<Self>::new();
         let mut neg_intervals = (-self).pow_p_a_multi(rhs);
@@ -109,34 +111,28 @@ impl<BOUND: Float> Interval<BOUND> {
     /// Computes `self` raised to the power `rhs` and returns a vector of intervals minimally
     /// covering the result.
     pub fn pow_multi(self, rhs: Self) -> Vec<Self> {
-        let precision = self.precision();
-
-        if rhs.is_nan() || self.is_nan() {
-            return vec![];
-        }
-        if rhs.is_zero() {
-            return vec![Self::one(precision)];
-        }
-        if self.is_zero() {
-            return if rhs.has_zero() {
+        if self.is_nan() || rhs.is_nan() {
+            vec![]
+        } else if rhs.is_singleton() {
+            self.pow_a_s_multi(rhs.hi)
+        } else if self.is_zero() {
+            if rhs.has_zero() {
                 vec![Self::zero(self.precision()), Self::one(self.precision())]
             } else {
                 vec![self]
-            };
+            }
+        } else {
+            let precision = self.precision();
+            let mut intervals = Vec::<Self>::new();
+            let (self_n, self_p) = self.split(BOUND::zero(precision));
+            if !self_p.is_nan() {
+                intervals.append(&mut self_p.pow_p_a_multi(rhs.clone()));
+            }
+            if !self_n.is_nan() {
+                intervals.append(&mut self_n.pow_n_a_multi(rhs));
+            }
+            intervals
         }
-        if rhs.is_singleton() {
-            return self.pow_a_s_multi(rhs.hi);
-        }
-
-        let mut intervals = Vec::<Self>::new();
-        let (self_n, self_p) = self.split(BOUND::zero(precision));
-        if !self_p.is_nan() {
-            intervals.append(&mut self_p.pow_p_a_multi(rhs.clone()));
-        }
-        if !self_n.is_nan() {
-            intervals.append(&mut self_n.pow_n_a_multi(rhs));
-        }
-        intervals
     }
 }
 
